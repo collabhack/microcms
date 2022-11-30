4e1ea0615fb3b401b2215875cc29c958c482ed42
Qijie Yu <qijie@issuefab.com>
2022-11-30 20:28:05 +0100

    model updates

--- model/index.ts
+++ model/index.ts
@@ -1,13 +1,26 @@
 import * as isoly from "isoly"
-
-export interface Message {
+import * as cryptly from "cryptly"
+export interface Message extends Creatable {
+	id: cryptly.Identifier
+	created: isoly.DateTime
+}
+export interface Creatable {
 	title: string
 	content: string
-	created: isoly.DateTime
 }
 export namespace Message {
 	export function is(value: any | Message): value is Message {
 		return value && typeof value == "object" && typeof value.title == "string" &&
-		 typeof value.content == "string"&& isoly.DateTime.is(value.created)  
+		 typeof value.content == "string" && isoly.DateTime.is(value.created) && cryptly.Identifier.is(value.id) 
+	}
+	export function create(value: Creatable): Message {
+		return {...value, id: cryptly.Identifier.generate(4), created: isoly.DateTime.now()}
 	}
 }
+	export namespace Creatable {
+		export function is(value: any | Creatable){
+			return value && typeof value == "object" && typeof value.title == "string" &&
+		 typeof value.content == "string"
+		}
+
+	}
