0b1952d2f98361f63195a437696140da7d0b547a
Linus Johansson <linus@issuefab.com>
2022-11-30 21:09:14 +0100

    Updated the message model.

--- model/index.ts
+++ model/index.ts
@@ -1,17 +1,20 @@
 import * as isoly from "isoly"
 import * as cryptly from "cryptly"
+
+
 export interface Message extends Creatable {
 	id: cryptly.Identifier
 	created: isoly.DateTime
 }
 export interface Creatable {
 	title: string
+	image?: string
 	content: string
 }
 export namespace Message {
 	export function is(value: any | Message): value is Message {
-		return value && typeof value == "object" && typeof value.title == "string" &&
-		 typeof value.content == "string" && isoly.DateTime.is(value.created) && cryptly.Identifier.is(value.id) 
+		return value && typeof value == "object" &&
+		 isoly.DateTime.is(value.created) && cryptly.Identifier.is(value.id) && Creatable.is(value)
 	}
 	export function create(value: Creatable): Message {
 		return {...value, id: cryptly.Identifier.generate(4), created: isoly.DateTime.now()}
@@ -20,7 +23,7 @@ export namespace Message {
 	export namespace Creatable {
 		export function is(value: any | Creatable){
 			return value && typeof value == "object" && typeof value.title == "string" &&
-		 typeof value.content == "string"
+			(value.image == undefined || typeof value.image == "string") && typeof value.content == "string"
 		}
 
 	}
--- ui/src/components.d.ts
+++ ui/src/components.d.ts
@@ -5,13 +5,21 @@
  * It contains typing information for all components that exist in this project.
  */
 import { HTMLStencilElement, JSXBase } from "@stencil/core/internal";
+import { Notice } from "smoothly";
 export namespace Components {
     interface MicrocmsCreate {
-        "feed": string;
     }
     interface MicrocmsList {
         "feed"?: string;
     }
+    interface SmoothlyInputFile {
+        "mode": "folder" | "file" | "files";
+        "name"?: string;
+    }
+}
+export interface MicrocmsCreateCustomEvent<T> extends CustomEvent<T> {
+    detail: T;
+    target: HTMLMicrocmsCreateElement;
 }
 declare global {
     interface HTMLMicrocmsCreateElement extends Components.MicrocmsCreate, HTMLStencilElement {
@@ -26,21 +34,33 @@ declare global {
         prototype: HTMLMicrocmsListElement;
         new (): HTMLMicrocmsListElement;
     };
+    interface HTMLSmoothlyInputFileElement extends Components.SmoothlyInputFile, HTMLStencilElement {
+    }
+    var HTMLSmoothlyInputFileElement: {
+        prototype: HTMLSmoothlyInputFileElement;
+        new (): HTMLSmoothlyInputFileElement;
+    };
     interface HTMLElementTagNameMap {
         "microcms-create": HTMLMicrocmsCreateElement;
         "microcms-list": HTMLMicrocmsListElement;
+        "smoothly-input-file": HTMLSmoothlyInputFileElement;
     }
 }
 declare namespace LocalJSX {
     interface MicrocmsCreate {
-        "feed"?: string;
+        "onNotice"?: (event: MicrocmsCreateCustomEvent<Notice>) => void;
     }
     interface MicrocmsList {
         "feed"?: string;
     }
+    interface SmoothlyInputFile {
+        "mode"?: "folder" | "file" | "files";
+        "name"?: string;
+    }
     interface IntrinsicElements {
         "microcms-create": MicrocmsCreate;
         "microcms-list": MicrocmsList;
+        "smoothly-input-file": SmoothlyInputFile;
     }
 }
 export { LocalJSX as JSX };
@@ -49,6 +69,7 @@ declare module "@stencil/core" {
         interface IntrinsicElements {
             "microcms-create": LocalJSX.MicrocmsCreate & JSXBase.HTMLAttributes<HTMLMicrocmsCreateElement>;
             "microcms-list": LocalJSX.MicrocmsList & JSXBase.HTMLAttributes<HTMLMicrocmsListElement>;
+            "smoothly-input-file": LocalJSX.SmoothlyInputFile & JSXBase.HTMLAttributes<HTMLSmoothlyInputFileElement>;
         }
     }
 }
